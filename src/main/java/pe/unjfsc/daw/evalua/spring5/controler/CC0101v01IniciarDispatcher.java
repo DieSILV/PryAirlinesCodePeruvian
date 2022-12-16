e37ed179e73b10dafc1930a50dafcae5c7d4d0a1
DieSILV <diego.silva.carrion1@hotmail.com>
2022-12-15 20:56:47 -0500

    Base Project

---
+++ src/main/java/pe/unjfsc/daw/evalua/spring5/controler/CC0101v01IniciarDispatcher.java
@@ -0,0 +1,54 @@
+package pe.unjfsc.daw.evalua.spring5.controler;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CC0101v01IniciarDispatcher {
+	public static final Logger LOG = LoggerFactory.getLogger("CC0101v01IniciarDispatcher");
+
+	@RequestMapping(value = "/contactos.pe")
+	public ModelAndView handleRequest_contactos(HttpServletRequest request, HttpServletResponse response) {
+		String lsArchivoJsp = "contactos"; // Define nombre del archivo jsp fisico sin la extension
+		String lsFechaSistema = (new Date()).toString();
+		LOG.info("[DAW] lsArchivoJsp: 	{}", lsArchivoJsp);
+		LOG.info("[DAW] lsFechaSistema        : {}", lsFechaSistema);
+		return new ModelAndView(lsArchivoJsp, "vFechaSistema", lsFechaSistema);
+	}
+	
+	@RequestMapping(value = "/lugares.pe")
+	public ModelAndView handleRequest_experiencias(HttpServletRequest request, HttpServletResponse response) {
+		String lsArchivoJsp = "lugares"; // Define nombre del archivo jsp fisico sin la extension
+		String lsFechaSistema = (new Date()).toString();
+		LOG.info("[DAW] lsArchivoJsp: 	{}", lsArchivoJsp);
+		LOG.info("[DAW] lsFechaSistema        : {}", lsFechaSistema);
+		return new ModelAndView(lsArchivoJsp, "vFechaSistema", lsFechaSistema);
+	}	
+
+	@RequestMapping(value = "/vuelos.pe")
+	public ModelAndView handleRequest_aviones(HttpServletRequest request, HttpServletResponse response) {
+		String lsArchivoJsp = "vuelos"; // Define nombre del archivo jsp fisico sin la extension
+		String lsFechaSistema = (new Date()).toString();
+		LOG.info("[DAW] lsArchivoJsp: 	{}", lsArchivoJsp);
+		LOG.info("[DAW] lsFechaSistema        : {}", lsFechaSistema);
+		return new ModelAndView(lsArchivoJsp, "vFechaSistema", lsFechaSistema);
+	}	
+	
+	@RequestMapping(value = "/hoteles.pe")
+	public ModelAndView handleRequest_lugares(HttpServletRequest request, HttpServletResponse response) {
+		String lsArchivoJsp = "hoteles"; // Define nombre del archivo jsp fisico sin la extension
+		String lsFechaSistema = (new Date()).toString();
+		LOG.info("[DAW] lsArchivoJsp: 	{}", lsArchivoJsp);
+		LOG.info("[DAW] lsFechaSistema        : {}", lsFechaSistema);
+		return new ModelAndView(lsArchivoJsp, "vFechaSistema", lsFechaSistema);
+	}
+	
+}
---
+++ src/main/java/pe/unjfsc/daw/evalua/spring5/entity/CEUsuario.java
@@ -0,0 +1,56 @@
+package pe.unjfsc.daw.evalua.spring5.entity;
+
+public class CEUsuario {
+	private int id;
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String usuario;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
